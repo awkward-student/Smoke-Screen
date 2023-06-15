e2606a4b3dcb9b943a3da837203f5e02db05173c
Vishwas <im.vishwas.03@gmail.com>
2023-06-16 03:14:32 +0530

    Create CustomFooter.jsx

---
+++ src/components/CustomFooter.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "reactstrap";
+
+const CustomFooter = () => {
+  return (
+    <div>
+      <Navbar className="bg-dark py-3" style={{height:"100%"}}>
+
+      </Navbar>
+    </div>
+  );
+};
+
+export default CustomFooter;
