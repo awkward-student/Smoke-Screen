ac806f5f5c7e61295141275857aa14fef35cca34
Vishwas <im.vishwas.03@gmail.com>
2023-06-16 01:56:57 +0530

    Create User Dashboard

---
+++ src/pages/dashboard.js
@@ -0,0 +1,13 @@
+import Base from "../components/Base";
+import Bisector from "../components/Bisector";
+
+
+const Dashboard = () => {
+    return(
+        <Base>
+            <Bisector />
+        </Base>
+    );
+}
+
+export default Dashboard;
