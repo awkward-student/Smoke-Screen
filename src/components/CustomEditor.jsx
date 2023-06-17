231942d6b2121b145d42539bd5a657ade273c7c2
Vishwas <im.vishwas.03@gmail.com>
2023-06-17 22:01:48 +0530

    Update CustomEditor.jsx

--- src/components/CustomEditor.jsx
+++ src/components/CustomEditor.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import { Editor } from "@tinymce/tinymce-react";
+import { Button } from "reactstrap";
 
 const CustomEditor = () => {
   const editorRef = useRef(null);
@@ -9,6 +10,8 @@ const CustomEditor = () => {
   //   }
   // };
 
+  const colour =
+    "body { background-color:black ;font-family:Helvetica,Arial,sans-serif; font-size:14px; color:transparent; user-select:none;-webkit-user-select:none;-webkit-touch-callout: none;-khtml-user-select: none; -moz-user-select: none;-ms-user-select: none;}";
   useEffect(() => {
     // define a custom handler function
     // for the contextmenu event
@@ -19,7 +22,7 @@ const CustomEditor = () => {
 
     // attach the event listener to
     // the document object
-    document.addEventListener("contextmenu", handleContextMenu);
+    // document.addEventListener("contextmenu", handleContextMenu);
 
     // clean up the event listener when
     // the component unmounts
@@ -29,14 +32,39 @@ const CustomEditor = () => {
   }, []);
   return (
     <>
-      <head></head>
       <Editor
         selector="textareaEdit"
         apiKey="bgx3j6j885xiq1toxfp8jbpr389dyji48oc6mejvg4s557un"
         onInit={(evt, editor) => (editorRef.current = editor)}
         initialValue=""
         init={{
-          height: 450,
+          browser_spellcheck: false,
+          height: 400,
+          width: 650,
+
+          setup: (editor) => {
+            editor.on("keydown", (e) => {
+              if (e.code == "F12") {
+                console.log("F12");
+                return false;
+              } else if (e.ctrlKey && e.code === "KeyA") {
+                console.log("CTRL + A");
+                alert("Text Selection Not Allowed\n\nReloading The Page....");
+                window.location.reload();
+                editor.setContent = "Your writings are Gone";
+                editor.initialValue = "Gone";
+                return false;
+              } else if (e.ctrlKey && e.code === "KeyS") {
+                console.log("CTRL + S");
+                alert("Text Saving Not Allowed");
+                return false;
+              } else if (e.ctrlKey && e.shiftKey && e.code == "KeyI") {
+                console.log("CTRL + SHIFT + I");
+                return false;
+              } else console.log("keydown press detected" + e.code);
+              // tinymce.activeEditor.execCommand("mceSpellcheckEnable");
+            });
+          },
           menubar: false,
           plugins: [],
           paste_preprocess: function (plugin, args) {
@@ -48,16 +76,15 @@ const CustomEditor = () => {
             args.content = "";
           },
           contextmenu_never_use_native: true,
-          contextmenu: null,
-          contextmenu: true,
+          // contextmenu: null,
+          contextmenu: false,
           toolbar: false,
           statusbar: false,
           auto_focus: true,
-          content_style:
-            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px; color:transparent; user-select:none;-webkit-user-select:none;-webkit-touch-callout: none;-khtml-user-select: none; -moz-user-select: none;-ms-user-select: none;}",
+          content_style: colour,
         }}
       />
-      {/* <button onClick={log}>Log editor content</button> */}
+      {/* <Button>Clibk</Button> */}
     </>
   );
 };
