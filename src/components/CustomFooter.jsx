69ac393fb1662f4ac344093acae4db2a76062384
Vishwas <im.vishwas.03@gmail.com>
2023-06-17 22:00:33 +0530

    Create CustomFooter.jsx

---
+++ src/components/CustomFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from 'reactstrap'
+
+const CustomFooter = () => {
+  return (
+    <Navbar color="dark" dark expand="md" fixed="bottom" className="" style={{minHeight:"4vh"}}>
+        
+    </Navbar>
+  )
+}
+
+export default CustomFooter
