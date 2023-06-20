ca5bb2e9aec7e37f5fc1d88c138f407c9420e8bb
Vishwas <im.vishwas.03@gmail.com>
2023-06-20 15:50:19 +0530

    Create helper.js

---
+++ src/services/helper.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { getToken } from "../auth/auth";
+
+export const BASE_URL = "http://localhost:8080";
+
+export const Axios = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const PRIVATE_AXIOS = axios.create({
+    baseURL:BASE_URL,
+})
+
+PRIVATE_AXIOS.interceptors.request.use((config) => {
+    const token = getToken();
+    if(token){
+        config.headers.Authorization='Bearer '+token;
+        console.log(config.headers.Authorization);
+        console.log(config);
+    }
+    return config;
+}, (error) => Promise.reject(error));
