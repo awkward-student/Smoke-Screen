bd68bb27bd45d830adce29a021cd208dd73eb288
Vishwas <im.vishwas.03@gmail.com>
2023-06-20 15:50:33 +0530

    Create admin-service.js

---
+++ src/services/admin-service.js
@@ -0,0 +1,13 @@
+import { Axios } from "./helper";
+
+export const getAllSubmissions = () => {
+  return Axios.get("v1/api/adm/auth/submissions").then((res) => {
+    return res.data;
+  });
+};
+
+export const getSubmissionByUser = (userId) => {
+    return Axios.get("v1/api/adm/auth/submission/user/"+userId).then((res) => {
+      return res.data;
+    });
+  };
