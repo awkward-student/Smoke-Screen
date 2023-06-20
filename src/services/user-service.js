1dbd5e7db2237f390831d162d2af7fe557ee53fc
Vishwas <im.vishwas.03@gmail.com>
2023-06-20 15:50:10 +0530

    Create user-service.js

---
+++ src/services/user-service.js
@@ -0,0 +1,11 @@
+import { Axios } from "./helper";
+
+export const RegisterUser = (userData) => {
+  return Axios.post("/v1/api/user/register", userData).then((res) => res.data);
+};
+
+export const LoginUser = (userCredentials) => {
+  return Axios.post("/v1/api/auth/login", userCredentials).then(
+    (res) => res.data
+  );
+};
