8ef35e36b476da1f79b2d8932ad1a19db264dff3
Vishwas <im.vishwas.03@gmail.com>
2023-06-29 16:26:00 +0530

    Update solution-service.js

--- src/services/solution-service.js
+++ src/services/solution-service.js
@@ -1,5 +1,6 @@
 import { PRIVATE_AXIOS } from "./helper";
 
+// Submit solution
 export const SubmitSolution = (solutions, userId) => {
   console.log(solutions);
   console.log(userId);
@@ -7,4 +8,11 @@ export const SubmitSolution = (solutions, userId) => {
     "v1/api/user/" + userId + "/submission",
     solutions
   ).then((res) => res.data);
+};
+
+// Get all solutions
+export const loadAllSubmissions = (pageNumber, pageSize) => {
+  return PRIVATE_AXIOS.get("v1/api/adm/auth/submissions?pageNumber="+pageNumber+"&pageSize="+pageSize).then(
+    (res) => res.data
+  );
 };
