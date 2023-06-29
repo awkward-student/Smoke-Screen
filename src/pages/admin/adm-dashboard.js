4134b63ee23ebc2c94e34d492c6d5d74c5865887
Vishwas <im.vishwas.03@gmail.com>
2023-06-29 16:25:47 +0530

    Update adm-dashboard.js

--- src/pages/admin/adm-dashboard.js
+++ src/pages/admin/adm-dashboard.js
@@ -1,33 +1,73 @@
-import React, { useRef } from "react";
+import React, { useEffect, useState } from "react";
 import Base from "../../components/Base";
-import { Card, CardGroup, CardHeader, Container, Label } from "reactstrap";
-import JoditEditor from "jodit-react";
 import "../../App.css";
+import { loadAllSubmissions } from "../../services/solution-service";
+import Solution from "../../components/Solution";
+import {
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Container,
+  Button,
+} from "reactstrap";
+import { toast } from "react-toastify";
+import { doLogout } from "../../auth/auth";
+import { useNavigate } from "react-router-dom";
 
 const AdminDashboard = () => {
-  const config = {
-    // buttons: null,
-    autofocus: true,
-    enter: "BR",
-    toolbar: false,
-    iframe: true,
-    showCharsCounter: false,
-    showWordsCounter: false,
-    showXPathInStatusbar: false,
-    height: 450,
-    minHeight: 450,
-    minWidth: 450,
-    allowResizeY: false,
-    allowResizeX: false,
-    color: "transparent",
-    readonly: true,
+  // Variable to store all solutions
+  const [solutions, setSolutions] = useState({
+    content: [],
+    pageNumber: "",
+    pageSize: "",
+    totalElements: "",
+    totalPages: "",
+    lastPage: false,
+  });
+
+  const navigate = useNavigate();
+
+  // Current page variable
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const changePage = (pageNumber = 0) => {
+    if (pageNumber > solutions.pageNumber && solutions.lastPage) return;
+    if (pageNumber < solutions.pageNumber && solutions.pageNumber == 0) return;
+    loadAllSubmissions(pageNumber, 1)
+      .then((data) => {
+        console.log(data);
+        setSolutions({
+          content: [...data.content],
+          pageNumber: data.pageNumber,
+          pageSize: data.pageSize,
+          totalElements: data.totalElements,
+          totalPages: data.totalPages,
+          lastPage: data.lastPage,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Error in loading submissions.");
+      });
   };
 
-  const editor = useRef(null);
+  const performLogout = () => {
+    doLogout(() => {
+      toast.info("Logged out successfully.");
+      navigate("/login");
+    });
+  };
 
-  var language1 = "java";
-  var language2 = "c++";
-  var language3 = "c++";
+  // Loading all submissions on load of the component
+  useEffect(() => {
+    changePage(0);
+  }, []);
+
+  const loginOperation = () => {};
+
+  const registerOperation = () => {};
+
+  // Main Page Body ----------------------------------------------
 
   return (
     <Base>
@@ -44,46 +84,51 @@ const AdminDashboard = () => {
           marginTop: "12px",
         }}
       >
-        <div className="mt-5 p-2 userInfo">
-          <Card>
-            User Name :{} User Email :{} Time Taken :{} Flash Count :{}{" "}
-            Submitted On :{}
-          </Card>
-        </div>
-        <div className="mt-4 userInfo">
-          <CardGroup className="">
-            <Card className="jodits">
-              <CardHeader>
-                Solution Que 1 : Language used - {language1}
-              </CardHeader>
-              <JoditEditor
-                ref={editor}
-                config={config}
-                className="joditStyle"
-              />
-            </Card>
-            <Card className="jodits">
-              <CardHeader>
-                Solution Que 2 : Language used - {language2}
-              </CardHeader>
-              <JoditEditor
-                ref={editor}
-                config={config}
-                className="joditStyle"
-              />
-            </Card>
-            <Card className="jodits">
-              <CardHeader>
-                Solution Que 3 : Language used - {language3}
-              </CardHeader>
-              <JoditEditor
-                ref={editor}
-                config={config}
-                className="joditStyle"
-              />
-            </Card>
-          </CardGroup>
-        </div>
+        {/* <h4>Total number of submissions : {solutions?.totalElements}</h4> */}
+        {solutions?.content.map((solution) => (
+          <Solution key={solution.id} solution={solution} />
+        ))}
+        {/* <Solution solution={solutions?.content}/> */}
+        <Container className="mt-3 text-center">
+          <Pagination>
+            <PaginationItem
+              onClick={() => changePage(solutions.pageNumber - 1)}
+              disabled={solutions.pageNumber === 0}
+            >
+              <PaginationLink previous></PaginationLink>
+            </PaginationItem>
+            {[...Array(solutions.totalPages)].map((item, index) => (
+              <PaginationItem
+                onClick={() => changePage(index)}
+                active={index === solutions.pageNumber}
+                key={index}
+              >
+                <PaginationLink>{index + 1}</PaginationLink>
+              </PaginationItem>
+            ))}
+            <PaginationItem
+              onClick={() => changePage(solutions.pageNumber + 1)}
+              disabled={solutions.lastPage}
+            >
+              <PaginationLink next></PaginationLink>
+            </PaginationItem>
+            <PaginationItem
+              onClick={() => changePage(solutions.totalPages - 1)}
+              disabled={solutions.lastPage}
+            >
+              <PaginationLink last></PaginationLink>
+            </PaginationItem>
+          </Pagination>
+          <div className="admOptions">
+            <Button color="primary" onClick={performLogout}>
+              Logout
+            </Button>
+            <span>
+              <Button onClick={loginOperation} id="loginOps"></Button>
+              <Button onClick={registerOperation} id="registerOps"></Button>
+            </span>
+          </div>
+        </Container>
       </Container>
     </Base>
   );
