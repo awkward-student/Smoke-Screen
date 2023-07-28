56e0f50d9c8b60e213760a5788924b90cd96a290
Vishwas <im.vishwas.03@gmail.com>
2023-07-28 13:17:12 +0530

    Create home.js

---
+++ src/pages/home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Base from "../components/Base";
+
+const Home = () => {
+  return (
+    <Base>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <p>Still to be designed after name confirmation</p>
+    </Base>
+  );
+};
+
+export default Home;
