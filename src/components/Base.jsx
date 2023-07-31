850eec392c2fb1e3e62501ebd0830ee61f13f67e
Vishwas <im.vishwas.03@gmail.com>
2023-07-31 18:50:59 +0530

    Update Base

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -6,9 +6,7 @@ const Base = ({ title = "Welcome to our website", children }) => {
   return (
     <div className="container-fluid p-0 m-0 base">
       <CustomNavbar />
-
       {children}
-
       <CustomFooter />
     </div>
   );
