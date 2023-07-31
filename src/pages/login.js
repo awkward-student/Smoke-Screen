1504d03068051c4ec458fa0b21792d7920f5bb3c
Vishwas <im.vishwas.03@gmail.com>
2023-07-31 18:52:33 +0530

    Update login.js/register.js Create formstyle.css
    Styled login and registration forms

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,15 +1,7 @@
 import React, { useState } from "react";
+import "./styles/formstyle.css";
 import Base from "../components/Base";
-import {
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Form,
-  FormGroup,
-  Input,
-} from "reactstrap";
+import { Form } from "reactstrap";
 import { toast } from "react-toastify";
 import { LoginUser } from "../services/user-service";
 import { doLogin, getCurrentUserProfile } from "../auth/auth";
@@ -19,14 +11,7 @@ import { useEffect } from "react";
 const Login = () => {
   const navigate = useNavigate();
 
-  //   useEffect(() => {
-  //     if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
-  //       toast.warning("Can not login twice!");
-  //     }
-  //   }, []);
-
   useEffect(() => {
-    // console.log(JSON.parse(localStorage.getItem("status"))[0].status);
     if (JSON.parse(localStorage.getItem("status"))[0].status === 0) {
       navigate("/register");
     }
@@ -43,7 +28,6 @@ const Login = () => {
   };
 
   //submit form
-  /* eslint-disable */
   const submitForm = (e) => {
     e.preventDefault();
     if (
@@ -53,6 +37,7 @@ const Login = () => {
       toast.error("Both username & password is required");
       return;
     }
+
     //submit data to server
     LoginUser(credentials)
       .then((res) => {
@@ -69,10 +54,6 @@ const Login = () => {
         console.log(error);
         if (error.message === "Network Error" && error.name === "AxiosError") {
           window.location.reload(true);
-          // toast.error("Login Failed : Can login only once.");
-          // setTimeout(() => {
-          //   window.location.reload(true);
-          // }, 0);
         }
         toast.error("Login Failed : " + error.response.data.message);
         setCredentials({
@@ -80,78 +61,34 @@ const Login = () => {
           password: "",
         });
       });
-    /* eslint-disable */
   };
 
   return (
     <Base>
-      <div
-        className=""
-        style={{
-          minHeight: "80vh",
-          minWidth: "90vw",
-          display: "flex",
-          alignSelf: "center",
-          alignContent: "center",
-          alignItems: "center",
-          justifyContent: "center",
-          justifyItems: "center",
-        }}
-      >
-        <Card
-          className="mt-4 text-center"
-          style={{
-            minHeight: "32vh",
-            minWidth: "25vw",
-            display: "flex",
-            alignSelf: "center",
-            alignContent: "center",
-            alignItems: "center",
-            justifyContent: "center",
-            justifyItems: "center",
-          }}
-        >
-          <CardHeader style={{ minWidth: "25vw" }}>USER LOGIN</CardHeader>
-          <CardBody
-            className="mt-2"
-            style={{ minHeight: "100px", maxHeight: "200px", minWidth: "25vw" }}
-          >
-            <Form onSubmit={submitForm}>
-              <FormGroup>
-                {/* <Label for="email">Username</Label> */}
-                <Input
-                  style={{ outline: "none" }}
-                  id="email"
-                  type="email"
-                  placeholder="user@name.here"
-                  onChange={(e) => handleChange(e, "username")}
-                  value={credentials.username}
-                ></Input>
-              </FormGroup>
-              <FormGroup>
-                {/* <Label for="password">Password</Label> */}
-                <Input
-                  style={{ outline: "none" }}
-                  id="password"
-                  type="password"
-                  placeholder="***********"
-                  onChange={(e) => handleChange(e, "password")}
-                  value={credentials.password}
-                ></Input>
-              </FormGroup>
-              <Button color="primary" className="primary">
-                Login
-              </Button>
-            </Form>
-          </CardBody>
-
-          <CardFooter style={{ minWidth: "25vw" }}>
-            <span style={{ color: "red", fontWeight: "200", fontSize: "13px" }}>
-              {" "}
-              * You can not login twice if taken assesment already *{" "}
-            </span>
-          </CardFooter>
-        </Card>
+      <div className="basediv">
+        <div className="formcard">
+          <Form onSubmit={submitForm}>
+            {/* <Label for="email">Username</Label> */}
+            <input
+              className="forminput"
+              id="email"
+              type="email"
+              placeholder="YOUR EMAIL"
+              onChange={(e) => handleChange(e, "username")}
+              value={credentials.username}
+            ></input>
+            {/* <Label for="password">Password</Label> */}
+            <input
+              className="forminput"
+              id="password"
+              type="password"
+              placeholder="YOUR PASSWORD"
+              onChange={(e) => handleChange(e, "password")}
+              value={credentials.password}
+            ></input>
+            <button className="forminput formbtn">REQUEST BLINDFOLD</button>
+          </Form>
+        </div>
       </div>
     </Base>
   );
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,16 +1,6 @@
 import React, { useState } from "react";
 import Base from "../components/Base";
-import {
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Container,
-  Form,
-  FormGroup,
-  Input,
-} from "reactstrap";
+import { Form, FormGroup } from "reactstrap";
 import { RegisterUser } from "../services/user-service";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
@@ -20,7 +10,6 @@ const Register = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // console.log(JSON.parse(localStorage.getItem("status"))[0].status);
     if (JSON.parse(localStorage.getItem("status"))[1].status === 0) {
       navigate("/login");
     }
@@ -66,114 +55,77 @@ const Register = () => {
 
   return (
     <Base>
-      <Container
-        className=""
-        style={{
-          minHeight: "80vh",
-          minWidth: "90vw",
-          display: "flex",
-          alignSelf: "center",
-          alignContent: "center",
-          alignItems: "center",
-          justifyContent: "center",
-          justifyItems: "center",
-        }}
-      >
-        <Card
-          className="mt-5 text-center"
-          style={{
-            minHeight: "32vh",
-            minWidth: "30vw",
-            display: "flex",
-            alignSelf: "center",
-            alignContent: "center",
-            alignItems: "center",
-            justifyContent: "center",
-            justifyItems: "center",
-          }}
-        >
-          {/* {JSON.stringify(userData)} */}
-          <CardHeader style={{ minWidth: "30vw" }}>
-            USER REGISTRATION
-          </CardHeader>
-          <CardBody style={{ minWidth: "30vw" }} className="mt-2">
-            <Form onSubmit={submitForm}>
-              <FormGroup>
-                <Input
-                  id="name"
-                  type="text"
-                  placeholder="Your Name"
-                  onChange={(e) => handleChange(e, "name")}
-                  value={userData.name}
-                  // required
-                ></Input>
-              </FormGroup>
-              <FormGroup>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="Your Email"
-                  onChange={(e) => handleChange(e, "email")}
-                  value={userData.email}
-                  // required
-                ></Input>
-              </FormGroup>
-              <FormGroup>
-                <Input
-                  id="password"
-                  type="password"
-                  placeholder="Create Password"
-                  onChange={(e) => handleChange(e, "password")}
-                  value={userData.password}
-                  // required
-                ></Input>
-              </FormGroup>
-              <FormGroup>
-                <Input
-                  id="branch"
-                  type="select"
-                  placeholder="Select Branch"
-                  // defaultValue={0}
-                  onChange={(e) => handleChange(e, "branch")}
-                  value={userData.branch}
-                  // required
-                >
-                  <option disabled value={0}>
-                    Select Branch
-                  </option>
-                  <option value={"MCA"}>MCA</option>
-                  <option value={"B.Tech"}>BTECH</option>
-                </Input>
-              </FormGroup>
-              <FormGroup>
-                <Input
-                  id="sem"
-                  type="select"
-                  placeholder="Select Sem"
-                  // defaultValue={0}
-                  onChange={(e) => handleChange(e, "sem")}
-                  value={userData.sem}
-                  // required
-                >
-                  <option disabled value={0}>
-                    Select Semester
-                  </option>
-                  <option value={"1"}>1</option>
-                  <option value={"2"}>2</option>
-                  <option value={"3"}>3</option>
-                  <option value={"4"}>4</option>
-                  <option value={"5"}>5</option>
-                  <option value={"6"}>6</option>
-                  <option value={"7"}>7</option>
-                  <option value={"8"}>8</option>
-                </Input>
-              </FormGroup>
-              <Button color="primary">Register</Button>
-            </Form>
-          </CardBody>
-          <CardFooter style={{ minWidth: "30vw" }}></CardFooter>
-        </Card>
-      </Container>
+      <div className="basediv">
+        <div className="formcard">
+          <Form onSubmit={submitForm}>
+            <FormGroup>
+              <input
+                id="name"
+                type="text"
+                placeholder="YOUR NAME"
+                onChange={(e) => handleChange(e, "name")}
+                value={userData.name}
+                className="forminput"
+              ></input>
+            </FormGroup>
+            <FormGroup>
+              <input
+                id="email"
+                type="email"
+                placeholder="YOUR EMAIL"
+                onChange={(e) => handleChange(e, "email")}
+                value={userData.email}
+                className="forminput"
+              ></input>
+            </FormGroup>
+            <FormGroup>
+              <input
+                id="password"
+                type="password"
+                placeholder="CREATE PASSWORD"
+                onChange={(e) => handleChange(e, "password")}
+                value={userData.password}
+                className="forminput"
+              ></input>
+            </FormGroup>
+            <FormGroup>
+              <select
+                id="branch"
+                onChange={(e) => handleChange(e, "branch")}
+                value={userData.branch}
+                className="forminput"
+              >
+                <option disabled value={0}>
+                  SELECT BRANCH
+                </option>
+                <option value={"MCA"}>MCA</option>
+                <option value={"B.Tech"}>BTECH</option>
+              </select>
+            </FormGroup>
+            <FormGroup>
+              <select
+                id="sem"
+                onChange={(e) => handleChange(e, "sem")}
+                value={userData.sem}
+                className="forminput"
+              >
+                <option disabled value={0}>
+                  SELECT SEMESTER
+                </option>
+                <option value={"1"}>1</option>
+                <option value={"2"}>2</option>
+                <option value={"3"}>3</option>
+                <option value={"4"}>4</option>
+                <option value={"5"}>5</option>
+                <option value={"6"}>6</option>
+                <option value={"7"}>7</option>
+                <option value={"8"}>8</option>
+              </select>
+            </FormGroup>
+            <button className="forminput formbtn">REGISTER</button>
+          </Form>
+        </div>
+      </div>
     </Base>
   );
 };
